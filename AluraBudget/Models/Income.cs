9baa49f81424670fb948de6c9c567336879f232c
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 17:53:12 -0300

    :card_file_box: db(core): Add Income Model
    Add Income Model

---
+++ AluraBudget/Models/Income.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AluraBudget.Models
+{
+    public class Income
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
