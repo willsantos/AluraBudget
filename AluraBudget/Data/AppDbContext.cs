6df6a00f139ec60597755f3ca018429a2e60c5ed
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 17:56:29 -0300

    :wrench: config(core): Add DbContext
    DbContext is for making connection with Database

---
+++ AluraBudget/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AluraBudget.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AluraBudget.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Outgoing> Outgoings { get; set; }
+    }
+}
