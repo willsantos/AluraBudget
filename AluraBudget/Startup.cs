abbfef446217fe2eb0f5a2d0df13851432ef20dc
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 17:48:47 -0300

    :wrench: config(core): Add database configuration files
    Setting up Database with Mysql

--- AluraBudget/Startup.cs
+++ AluraBudget/Startup.cs
@@ -1,7 +1,9 @@
+using AluraBudget.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,7 @@ namespace AluraBudget
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySQL(Configuration.GetConnectionString("ApiConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
