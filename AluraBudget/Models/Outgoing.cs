39932fdb7aa67b890d3ca5378a2e32ec8fe5c8e2
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 17:54:15 -0300

    :card_file_box: db(core): Add Outgoing Model
    Add Outgoing Model

---
+++ AluraBudget/Models/Outgoing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AluraBudget.Models
+{
+    public class Outgoing
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
