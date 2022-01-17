e2d7aa41b445767475bc3e62b454fc92101dc06f
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 19:26:06 -0300

    :card_file_box: db: Add DTO to CreateIncomeDto
    Add DTO without id

---
+++ AluraBudget/Data/DTO/IncomeDto/CreateIncomeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AluraBudget.Data.DTO.IncomeDto
+{
+    public class CreateIncomeDto
+    {
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
