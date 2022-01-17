cd283f0f9f0a2bbb09198076a863b1e0a0581bd5
Wilson Santos <contato@wilsonsantos.net>
2022-01-17 17:58:42 -0300

    :card_file_box: db(Database): Add migration Files
    Create migrations
     add migration to create Income and Outgoing tables

---
+++ AluraBudget/Migrations/20220117202138_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AluraBudget.Migrations
+{
+    public partial class DbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
