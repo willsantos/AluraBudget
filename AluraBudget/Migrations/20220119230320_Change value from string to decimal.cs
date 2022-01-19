0a0959417aeaa75d9b2b1224f42df1ab09ee17e8
Wilson Santos <contato@wilsonsantos.net>
2022-01-19 20:40:42 -0300

    :card_file_box: db(database): Change type from Value field
    Change Value type from String to Decimal

---
+++ AluraBudget/Migrations/20220119230320_Change value from string to decimal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AluraBudget.Migrations
+{
+    public partial class Changevaluefromstringtodecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Value",
+                table: "Outgoings",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Outgoings",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
