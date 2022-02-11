1ad6b9d7a11736e55ae1119675b5f07afdd32473
Wilson Santos <contato@wilsonsantos.net>
2022-02-11 15:36:07 -0300

    :lock: security: Remove hardcoded credentials
    Use secrets, this is similar to .ENV
    I reset credentials from mailtrap after

--- UsersApi/Program.cs
+++ UsersApi/Program.cs
@@ -21,6 +21,8 @@ namespace UsersApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureAppConfiguration((context,builder)=>
+                    builder.AddUserSecrets<Program>());
     }
 }
